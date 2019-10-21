31e1f9b71b220afc80eaa195d09cab823e2b569a
Ula Latush <ula2sh@gmail.com>
2019-10-21 13:41:33 +0300

    photo viewer

--- js/main.js
+++ js/main.js
@@ -201,3 +201,64 @@ var resizePicture = function (scale) {
   };
 
 })();
+
+// open big picture
+document.addEventListener('click', function (evt) {
+  if (evt.srcElement.classList.contains('picture__img')) {
+    window.photoViewer.open(evt.srcElement.src);
+  }
+});
+
+// close big picture
+document.querySelector('.big-picture__cancel').addEventListener('click', window.photoViewer.close);
+
+function findPictureByUrl(url) {
+
+  for (var i = 0; i < window.photoData.length; i++) {
+    if (url.includes(window.photoData[i].url)) {
+      return window.photoData[i];
+    }
+  }
+  return null;
+}
+
+(function () {
+
+  function createCommentElement(comment, commentTemplate) {
+    var instance = commentTemplate.cloneNode(true);
+    instance.querySelector('img').setAttribute('src', comment.avatar);
+    instance.querySelector('p').textContent = comment.message;
+    instance.querySelector('img').setAttribute('alt', comment.name);
+    return instance;
+  }
+
+  window.photoViewer = {
+
+    open: function (url) {
+
+      var picture = findPictureByUrl(url);
+      var bigPicture = document.querySelector('.big-picture');
+      bigPicture.classList.remove('hidden');
+      // fill likes, comments amount and description
+      bigPicture.querySelector('.big-picture__img').children[0].setAttribute('src', picture.url);
+      bigPicture.querySelector('.likes-count').textContent = picture.likes;
+      bigPicture.querySelector('.comments-count').textContent = picture.comments.length;
+      bigPicture.querySelector('.social__caption').textContent = picture.description;
+      // add comment elements to page
+      var commentsContainer = bigPicture.querySelector('.social__comments');
+      var commentTemplate = commentsContainer.children[0];
+      var commentsFragment = document.createDocumentFragment();
+      for (var c = 0; c < picture.comments.length; c++) {
+        commentsFragment.appendChild(createCommentElement(picture.comments[c], commentTemplate));
+      }
+      commentsContainer.innerHTML = '';
+      commentsContainer.appendChild(commentsFragment);
+      // hide blocks
+      bigPicture.querySelector('.social__comment-count').classList.add('visually-hidden');
+      bigPicture.querySelector('.comments-loader').classList.add('visually-hidden');
+    },
+    close: function () {
+      document.querySelector('.big-picture').classList.add('hidden');
+    }
+  };
+})();
